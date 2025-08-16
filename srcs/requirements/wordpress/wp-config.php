89a9174dbe4888f9a1413a2da52bd2d59e972519
Abdellatif El Fagrouch <ael-fagr@e3r1p5.1337.ma>
2025-08-16 17:21:53 +0100

    .

---
+++ srcs/requirements/wordpress/wp-config.php
@@ -0,0 +1,11 @@
+<?php
+
+//define database name
+define("DB_NAME", "name");
+//define user
+define("DB_USER", "username");
+//define database password
+define("DB_PASSWORD", "pass");
+//define host
+define("DB_HOST", "localhost");
+>
